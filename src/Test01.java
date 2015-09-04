fbc7a7e239e13159578b884705330a86cd93bf8e
unknown <x15g032@PC505N004.chiba-fjb.ac.jp>
2015-09-04 12:03:08 +0900

    さらに修正

--- src/Test01.java
+++ src/Test01.java
@@ -3,7 +3,9 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		System.out.println("バロス");
+		System.out.println("FPSは遊びじゃないんだよ！！");
+		System.out.println("干物妹うまるちゃん。");
 		
 	}
 
